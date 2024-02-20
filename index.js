74f1bf4364507cf962a92714b1ebaf7846510744
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 22:23:17 +0700

    redirect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { onClick,getValue } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {postWithToken} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
 if (getCookie("login")===""){
     redirect("/");
