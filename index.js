a03b9103aa7b7673db6e2e0f06be52f3d811d606
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 22:50:09 +0700

    coba

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ if (getCookie("login")===""){
 }
 
 onClick("saveForm",actionfunctionname);
+onClick("copyurl",actionCopy);
+
+function actionCopy(){
+    // Get the text field
+  var copyText = document.getElementById("urllaporan");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+}
 
 function actionfunctionname(){
     let laporan={
