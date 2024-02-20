e3e68afbfe44c3306308ab79fc91d58c6d651a5a
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 22:43:44 +0700

    getWithHeader

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { onClick,getValue,hide,show,setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
-import {postWithToken} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import {postWithToken,getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
