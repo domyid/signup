f86a35d835974b56fbf70f285c16b0d035b40104
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-20 22:39:35 +0700

    coba

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { onClick,getValue } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import { onClick,getValue,hide,show,setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {postWithToken} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
+
+hide("linkarea");
 if (getCookie("login")===""){
     redirect("/");
+}else{
+    getWithHeader("https://mrt.ulbi.ac.id/notif/ux/getdatauser","login",getCookie("login"),getUserFunction)
 }
 
 onClick("saveForm",actionfunctionname);
@@ -16,11 +20,17 @@ function actionfunctionname(){
         phone:getValue("phone"),
         solusi:getValue("solusi")
     };
-
+    hide("contentform");
     postWithToken("https://mrt.ulbi.ac.id/notif/ux/postlaporan","login",getCookie("login"),laporan,responseFunction);
-
 }
 
 function responseFunction(result){
+    show("linkarea");
+    setInner("urllaporan",result.data);
     console.log(result);
+}
+
+
+function getUserFunction(result){
+    setInner("header","Petugas : "+result.nama);
 }
