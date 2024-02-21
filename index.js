50beee34e58577355fcd7b13e0d6fd01432cc69d
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-21 16:18:07 +0700

    oke sip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { onClick,getValue,hide,show,setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+import { onClick,getValue,setValue,hide,show,setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {postWithToken,getWithHeader} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
@@ -25,13 +25,29 @@ function actionfunctionname(){
         phone:getValue("phone"),
         solusi:getValue("solusi")
     };
-    hide("contentform");
+    hide("saveForm");
     postWithToken("https://mrt.ulbi.ac.id/notif/ux/postlaporan","login",getCookie("login"),laporan,responseFunction);
 }
 
 function responseFunction(result){
-    show("linkarea");
-    setInner("urllaporan","Silahkan meminta user untuk melakukan rating pekerjaan yang dikirim ke nomor whatsapp yang diinputkan. #"+result.wa);
+    if("wa" in result){
+        Swal.fire({
+            icon: "success",
+            title: "Oops...",
+            text: "Ingatkan user "+result.user+" https://wa.me/"+result.wa+" untuk melakukan penilaian dari WA Iteung"
+          });
+          setValue("no","");
+          setValue("nama","");
+          setValue("phone","");
+          setValue("solusi","");
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: result.error
+          });
+          show("saveForm");
+    }
     console.log(result);
 }
 
